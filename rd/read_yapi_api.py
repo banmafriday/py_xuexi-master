eb783aab956c1b85bc3654d33f46c2850d677494
banma <294350757@qq.com>
2021-10-15 17:35:09 +0800

    ADD _resd-api

--- rd/read_yapi_api.py
+++ rd/read_yapi_api.py
@@ -1,24 +1,19 @@
 # coding:utf-8
 import requests
 import re
+import json
+
+s = requests.session()
 
 
 def login():
     '''登录'''
     url = "https://yapi.lessoald.cn/api/user/login"
-    qu = {"email": "294350757@qq.com", "password": "13975943593"}
 
-    headersw = {
-        'Host': 'yapi.lessoald.cn',
-        'content-type': "mapplication/json;charset=UTF-8",
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
-        'Accept': "application/json, text/plain, */*",
-        'host': 'yapi.lessoald.cn',
-        'Accept-Encoding': "gzip, deflate",
-        'Content-Length': "320",
-        'Connection': "keep-alive",
-        'cache-control': "no-cache"
-    }
+    d = json.dumps({
+        "email": "294350757@qq.com",
+        "password": "13975943593",
+    })
     headers = {
 
         'Host': 'yapi.lessoald.cn',
@@ -35,8 +30,32 @@ def login():
         'Referer': 'https://yapi.lessoald.cn/login',
 
     }
-    response = requests.post(url, headers=headers, params=qu)
-    print(response.text)
-
+    response = s.post(url, headers=headers, data=d)
+    result = response.cookies
+    print(result)
+    se= result.move("3")
 
 login()
+
+def Add_api():
+    # https: // yapi.lessoald.cn / group / 238
+    url = "https://yapi.lessoald.cn/group/238"
+
+    headers = {
+
+        'Host': 'yapi.lessoald.cn',
+        'Connection': 'keep-alive',
+        'Content-Length': '53',
+        'Accept': 'application/json, text/plain, */*',
+        'sec-ch-ua-mobile': '?0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Origin': 'https://yapi.lessoald.cn',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://yapi.lessoald.cn/login',
+
+    }
+
+    response = s.get(url, headers=headers)
